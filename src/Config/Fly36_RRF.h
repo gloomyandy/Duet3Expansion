082b42736a965e18124a8ba2dab218676afd6036
Andy Shaw <andy-git@gloomy-place.com>
2023-04-22 17:19:55 +0100

    Added support for new rev of Fly36RRF

--- src/Config/Fly36_RRF.h
+++ src/Config/Fly36_RRF.h
@@ -64,12 +64,21 @@ constexpr float MaximumMotorCurrent = 1600.0;
 constexpr float MaximumStandstillCurrent = 1200.0;
 constexpr uint32_t DefaultStandstillCurrentPercent = 75;
 
+#if BOARD_REV == 1
+constexpr Pin GlobalTmc22xxEnablePin = GpioPin(13);
+constexpr Pin Tmc22xxUartPin = GpioPin(15);
+
+constexpr Pin StepPins[NumDrivers] = { GpioPin(7) };
+constexpr Pin DirectionPins[NumDrivers] = { GpioPin(6) };
+constexpr Pin DriverDiagPins[NumDrivers] = { GpioPin(14) };
+#else
 constexpr Pin GlobalTmc22xxEnablePin = GpioPin(25);
 constexpr Pin Tmc22xxUartPin = GpioPin(1);
 
 constexpr Pin StepPins[NumDrivers] = { GpioPin(24) };
 constexpr Pin DirectionPins[NumDrivers] = { GpioPin(23) };
 constexpr Pin DriverDiagPins[NumDrivers] = { GpioPin(22) };
+#endif
 
 #define ACTIVE_HIGH_STEP		1		// 1 = active high, 0 = active low
 #define ACTIVE_HIGH_DIR			1		// 1 = active high, 0 = active low
@@ -94,25 +103,49 @@ constexpr size_t MaxPortsPerHeater = 1;
 constexpr size_t NumThermistorInputs = 2;
 constexpr float DefaultThermistorSeriesR = 4700.0;		// TEMP0 has 1K or 4K7 pullup, chamber thermistor has 4K7
 
+#if BOARD_REV == 1
+constexpr Pin TempSensePins[NumThermistorInputs] = { GpioPin(27), GpioPin(28) };
+
+constexpr Pin CanTxPin = GpioPin(NoPin);
+constexpr Pin CanRxPin = GpioPin(NoPin);
+#else
 constexpr Pin TempSensePins[NumThermistorInputs] = { GpioPin(26), GpioPin(27) };
 
 constexpr Pin CanTxPin = GpioPin(5);
 constexpr Pin CanRxPin = GpioPin(4);
+#endif
 
 constexpr Pin ButtonPins[] = { PIN_TODO };
 
 // VIN voltage monitor
+#if BOARD_REV == 1
+constexpr Pin VinMonitorPin = GpioPin(29);
+#else
 constexpr Pin VinMonitorPin = GpioPin(28);
+#endif
 constexpr float VinDividerRatio = (47.0 + 4.7)/4.7;
 constexpr float VinMonitorVoltageRange = VinDividerRatio * 3.3;				// the Pico uses the 3.3V supply as the voltage reference
 
 // Diagnostic LEDs
+#if BOARD_REV == 1
+constexpr Pin LedPins[] = { GpioPin(12) };
+#else
 constexpr Pin LedPins[] = { GpioPin(20) };
+#endif
 constexpr bool LedActiveHigh = false;
 
 #if SUPPORT_SPI_SENSORS
 
 // Shared SPI pin connections
+#if BOARD_REV == 1
+constexpr uint8_t SspiSpiInstanceNumber = 0;
+constexpr Pin SSPIMosiPin = GpioPin(3);
+constexpr GpioPinFunction SSPIMosiPinPeriphMode = GpioPinFunction::Spi;
+constexpr Pin SSPISclkPin = GpioPin(2);
+constexpr GpioPinFunction SSPISclkPinPeriphMode = GpioPinFunction::Spi;
+constexpr Pin SSPIMisoPin = GpioPin(4);
+constexpr GpioPinFunction SSPIMisoPinPeriphMode = GpioPinFunction::Spi;
+#else
 constexpr uint8_t SspiSpiInstanceNumber = 1;
 constexpr Pin SSPIMosiPin = GpioPin(11);
 constexpr GpioPinFunction SSPIMosiPinPeriphMode = GpioPinFunction::Spi;
@@ -120,18 +153,60 @@ constexpr Pin SSPISclkPin = GpioPin(10);
 constexpr GpioPinFunction SSPISclkPinPeriphMode = GpioPinFunction::Spi;
 constexpr Pin SSPIMisoPin = GpioPin(12);
 constexpr GpioPinFunction SSPIMisoPinPeriphMode = GpioPinFunction::Spi;
-
+#endif
 #endif
 
 #if SUPPORT_LIS3DH
 
 #define ACCELEROMETER_USES_SPI			(1)					// 0 if the accelerometer is connected via I2C, 1 if via SPI
+#if BOARD_REV == 1
+constexpr Pin Lis3dhCsPin = GpioPin(1);
+constexpr Pin Lis3dhInt1Pin = GpioPin(25);
+#else
 constexpr Pin Lis3dhCsPin = GpioPin(9);
 constexpr Pin Lis3dhInt1Pin = GpioPin(29);
+#endif
 
 #endif
 
 // Table of pin functions that we are allowed to use
+#if BOARD_REV == 1
+constexpr PinDescription PinTable[] =
+{
+	//	PWM					ADC				PinName
+	// Port A
+	{ PwmOutput::pwm0a,	AdcInput::none,		"out1"		},	// GPIO0 Fan0
+	{ PwmOutput::pwm0b,	AdcInput::none,		nullptr		},	// GPIO1 SPI ACC CS
+	{ PwmOutput::pwm1a,	AdcInput::none,		nullptr		},	// GPIO2 SPI0 SCK
+	{ PwmOutput::pwm1b,	AdcInput::none,		nullptr		},	// GPIO3 SPI0 MOSI
+	{ PwmOutput::pwm2a,	AdcInput::none,		nullptr		},	// GPIO4 SPI0 MISO
+	{ PwmOutput::pwm2b,	AdcInput::none,		nullptr		},	// GPIO5 SPI SA5047D CS
+	{ PwmOutput::pwm3a,	AdcInput::none,		nullptr		},	// GPIO6 DIR
+	{ PwmOutput::pwm3b,	AdcInput::none,		nullptr		},	// GPIO7 STEP
+	{ PwmOutput::pwm4a,	AdcInput::none,		nullptr		},	// GPIO8 SPI1 MISO
+	{ PwmOutput::pwm4b,	AdcInput::none,		nullptr		},	// GPIO9 SPI1 CS
+	{ PwmOutput::pwm5a,	AdcInput::none,		nullptr		},	// GPIO10 SPI1 SCK
+	{ PwmOutput::pwm5b,	AdcInput::none,		nullptr		},	// GPIO11 SPI1 MOSI
+	{ PwmOutput::pwm6a,	AdcInput::none,		nullptr 	},	// GPIO12 Status LED
+	{ PwmOutput::pwm6b,	AdcInput::none,		nullptr		},	// GPIO13 EN
+	{ PwmOutput::pwm7a,	AdcInput::none,		nullptr		},	// GPIO14 DIAG
+	{ PwmOutput::pwm7b,	AdcInput::none,		nullptr		},	// GPIO15 TMC UART
+	{ PwmOutput::pwm0a,	AdcInput::none,		"io2.in"	},	// GPIO16 ENDSTOP2
+	{ PwmOutput::pwm0b,	AdcInput::none,		"max31865cs,rtdcs"		},	// GPIO17 SPI MAX31856 CS
+	{ PwmOutput::pwm1a,	AdcInput::none,		nullptr 	},	// GPIO18 I2C SDA
+	{ PwmOutput::pwm1b,	AdcInput::none,		nullptr		},	// GPIO19 I2C SCL
+	{ PwmOutput::pwm2a,	AdcInput::none,		"io1.in"	},	// GPIO20 ENDSTOP1
+	{ PwmOutput::pwm2b,	AdcInput::none,		"out2"		},	// GPIO21 Fan1
+	{ PwmOutput::pwm3a,	AdcInput::none,		"io0.in"	},	// GPIO22 Probe
+	{ PwmOutput::pwm3b,	AdcInput::none,		"out0"		},	// GPIO23 Heat0
+	{ PwmOutput::pwm4a,	AdcInput::none,		"io0.out"	},	// GPIO24 Servo
+	{ PwmOutput::pwm4b,	AdcInput::none,		nullptr		},	// GPIO25 ACC INT1
+	{ PwmOutput::pwm5a,	AdcInput::adc0_0,	"rgbled"	},	// GPIO26 RGBLED
+	{ PwmOutput::pwm5b,	AdcInput::adc0_1,	"temp0"		},	// GPIO27 T0 Temp
+	{ PwmOutput::pwm6a,	AdcInput::adc0_2,	"temp1"		},	// GPIO28 CHAMBER_TEMP
+	{ PwmOutput::pwm6b,	AdcInput::adc0_3,	nullptr		},	// GPIO29 Power ADC
+};
+#else
 //TODO restrict each of pwm0 to pwm7 to just one output, to prevent users trying to use the same PWM unit for more than one pin
 constexpr PinDescription PinTable[] =
 {
@@ -168,6 +243,7 @@ constexpr PinDescription PinTable[] =
 	{ PwmOutput::pwm6a,	AdcInput::adc0_2,	nullptr		},	// GPIO28 VIN ADC
 	{ PwmOutput::none,	AdcInput::adc0_3,	nullptr		},	// GPIO29 ACC_INT1
 };
+#endif
 
 static constexpr size_t NumPins = ARRAY_SIZE(PinTable);
 static_assert(NumPins == 30);		// 30 GPIO pins on RP2040
@@ -205,6 +281,16 @@ const NvicPriority NvicPriorityAdc = 3;
 const NvicPriority NvicPriorityUSB = 3;
 
 #if SUPPORT_CAN && USE_SPICAN
+#if BOARD_REV == 1
+constexpr uint8_t spiCanSpiInstanceNumber = 1;
+constexpr Pin SPICanMosiPin = GpioPin(11);
+constexpr GpioPinFunction SPICanMosiPinPeriphMode = GpioPinFunction::Spi;
+constexpr Pin SPICanSclkPin = GpioPin(10);
+constexpr GpioPinFunction SPICanSclkPinPeriphMode = GpioPinFunction::Spi;
+constexpr Pin SPICanMisoPin = GpioPin(8);
+constexpr GpioPinFunction SPICanMisoPinPeriphMode = GpioPinFunction::Spi;
+constexpr Pin SPICanCsPin = GpioPin(9);
+#else
 constexpr uint8_t spiCanSpiInstanceNumber = 0;
 constexpr Pin SPICanMosiPin = GpioPin(19);
 constexpr GpioPinFunction SPICanMosiPinPeriphMode = GpioPinFunction::Spi;
