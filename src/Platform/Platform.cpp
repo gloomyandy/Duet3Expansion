50281e6c46587b7d53aea1d4c1c64ea5ef799721
Andy Shaw <andy-git@gloomy-place.com>
2023-06-08 13:23:13 +0100

    Add Fly SB2040v1_0

--- src/Platform/Platform.cpp
+++ src/Platform/Platform.cpp
@@ -562,7 +562,7 @@ namespace Platform
 		return (switches == 0) ? CanId::ExpansionBoardFirmwareUpdateAddress : switches;
 #elif defined(TOOL1LC)
 		return CanId::ToolBoardDefaultAddress;
-#elif defined(SAMMYC21) || defined(RPI_PICO) || defined(FLY36RRF)
+#elif defined(SAMMYC21) || defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 		return CanId::SammyC21DefaultAddress;
 #elif defined(EXP1XD)
 		return CanId::Exp1XDBoardDefaultAddress;
@@ -715,7 +715,7 @@ void Platform::Init()
 
 #if defined(SAMMYC21)
 	uart0.begin(115200);						// set up the UART with the same baud rate as the bootloader
-#elif defined(RPI_PICO) || defined(FLY36RRF)
+#elif defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 	serialUSB.Start(NoPin);
 #elif defined(DEBUG)
 	// Set up the UART to send to PanelDue for debugging
@@ -1343,12 +1343,12 @@ void Platform::Spin()
 		}
 	}
 
-#if defined(SAMMYC21) || defined(RPI_PICO) || defined(FLY36RRF)
+#if defined(SAMMYC21) || defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 	//debugPrintf("IR=%" PRIx32 " ERR=%" PRIx32 " RXF0S=%" PRIx32 " RXF1S=%" PRIx32 " PSR=%" PRIx32 " CCCR=%" PRIx32 "\n",
 	//	CAN0->IR.reg, CAN0->ECR.reg, CAN0->RXF0S.reg, CAN0->RXF1S.reg, CAN0->PSR.reg, CAN0->CCCR.reg);
 
 	// If D is received from the USB port, output some diagnostics
-# if defined(RPI_PICO) || defined(FLY36RRF)
+# if defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 	while (serialUSB.available() != 0)
 # elif defined(SAMMYC21)
 	while (uart0.available() != 0)
@@ -1356,7 +1356,7 @@ void Platform::Spin()
 	{
 # if defined(SAMMYC21)
 		const char c = uart0.read();
-# elif defined(RPI_PICO) || defined(FLY36RRF)
+# elif defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 		const char c = serialUSB.read();
 # endif
 		if (c == 'D')
@@ -1487,7 +1487,7 @@ bool Platform::DebugPutc(char c) noexcept
 {
 	if (c != 0)
 	{
-#if defined(RPI_PICO) || defined(FLY36RRF)
+#if defined(RPI_PICO) || defined(FLY36RRF) || defined(FLYSB2040v1_0)
 		serialUSB.write(c);
 #else
 		uart0.write(c);
