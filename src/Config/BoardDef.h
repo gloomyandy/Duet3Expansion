dbe3d4731153ba99370d5881df35b88c0e96e21b
Andy Shaw <andy-git@gloomy-place.com>
2023-08-20 14:26:16 +0100

    Add initial PITB support

--- src/Config/BoardDef.h
+++ src/Config/BoardDef.h
@@ -31,6 +31,8 @@
 # include "Fly36_RRF.h"
 #elif defined(FLYSB2040v1_0)
 # include "FlySB2040v1_0.h"
+#elif defined(PITBv1_0)
+# include "PITBv1_0.h"
 #elif defined(M23CL)
 # include "M23CL.h"
 #elif defined(SZP)
