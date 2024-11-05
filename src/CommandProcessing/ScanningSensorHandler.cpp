c761cf04ecae653a9ddd7345f3093babcc24ec45
Andy Shaw <andy-git@gloomy-place.com>
2024-11-05 16:07:35 +0000

    Make handling of scanning probes a little more generic

--- src/CommandProcessing/ScanningSensorHandler.cpp
+++ src/CommandProcessing/ScanningSensorHandler.cpp
@@ -27,7 +27,7 @@ static CallbackParameter callbackParameter;
 // This hook function is called by the AnalogIn task
 static void LDC1612TaskHook() noexcept
 {
-#if defined(SHT36) || defined(FLYSB2040V3_0)
+#if defined(SHT36) || defined(FLYSB2040V3_0) || defined(FYSETCSB2040V2)
 	// There is no interrupt pin available so sample every 2ms
 	if (!isCalibrating && millis() - lastReadingTakenAt > 1)
 #else
@@ -63,7 +63,7 @@ void ScanningSensorHandler::Init(SharedI2CMaster& i2cDevice) noexcept
 {
 	// Set up the external clock to the LDC1612.
 	// The higher the better, but the maximum is 40MHz
-#if defined(SAMMYC21) || defined(TOOL1LC) || defined(SHT36) || defined(FLYSB2040V3_0)
+#if defined(SAMMYC21) || defined(TOOL1LC) || defined(SHT36) || defined(FLYSB2040V3_0) || defined(FYSETCSB2040V2)
 	// Assume we are using a LDC1612 breakout board with its own crystal, so we don't need to generate a clock
 #elif defined(SZP)
 	// We can use the 96MHz DPLL output divided by 3 to get 32MHz but it is probably better to use 25MHz from the crystal directly for better stability.
@@ -97,10 +97,12 @@ void ScanningSensorHandler::Init(SharedI2CMaster& i2cDevice) noexcept
 	if (sensor->CheckPresent())
 	{
 		sensor->SetDefaultConfiguration(0, false);
-#if !defined(SHT36) || defined(FLYSB2040V3_0)
-		pinMode(LDC1612InterruptPin, PinMode::INPUT);
+#if defined(SHT36) && (BOARD_REV == 300)
+		// we don't have the INT pin hooked up on this board, so we just use a time based poll
+		lastReadingTakenAt = millis();
 #else
 		lastReadingTakenAt = millis();
+		pinMode(LDC1612InterruptPin, PinMode::INPUT);
 #endif
 		oldHookFunction = AnalogIn::SetTaskHook(LDC1612TaskHook);
 	}
