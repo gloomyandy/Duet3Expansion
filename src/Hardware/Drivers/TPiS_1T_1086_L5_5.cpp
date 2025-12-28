42690bea0f6171aee4a893706b80c3bfa676aae2
David Crocker <dcrocker@eschertech.com>
2025-12-28 08:49:43 +0000

    More work on TPiS driver

--- src/Hardware/Drivers/TPiS_1T_1086_L5_5.cpp
+++ src/Hardware/Drivers/TPiS_1T_1086_L5_5.cpp
@@ -9,38 +9,87 @@
 
 #if SUPPORT_TPiS_1T_1086_L5_5
 
-// Sensor type descriptors
-TemperatureSensor::SensorTypeDescriptor TPiS_1T_1086_L5_5::typeDescriptor(TypeName, [](unsigned int sensorNum) noexcept -> TemperatureSensor *_ecv_from { return new TPiS_1T_1086_L5_5(sensorNum); } );
+#include <CanMessageGenericParser.h>
 
-TPiS_1T_1086_L5_5::TPiS_1T_1086_L5_5(unsigned int sensorNum)
-	: I2CTemperatureSensor(sensorNum, "Thermopile (TPiS_1T_1086_L5_5)", TPiS_I2CChannel, TPiS_I2CAddress)
+TPiS_1T_1086_L5_5::TPiS_1T_1086_L5_5(SharedI2CMaster& master)
+	: device(master, TPiS_I2CAddress)
 {
 }
 
 // Configure this temperature sensor
-GCodeResult TPiS_1T_1086_L5_5::Configure(const CanMessageGenericParser& parser, const StringRef& reply)
+void TPiS_1T_1086_L5_5::Init() noexcept
 {
-	bool seen = false;
-	if (false)			//TODO call the configuration function
+	// Initialise the sensor
+	bool ok;
+	// Set the slave address of the sensor. After power up it on;y responds to the General Call address.
 	{
-		return GCodeResult::error;
+		device.SetAddress(0);									// general call address
+		const uint8_t txBuffer[] = { 0x04 };					// command to load address from EPROM
+		ok = device.Transfer(txBuffer, nullptr, sizeof(txBuffer), 0, TPis_I2C_timeout, true);
 	}
 
-	if (seen)
+	// Set up to read the EPROM
+	if (ok)
 	{
-		// Initialise the sensor
-		//TODO
+		delay(10);
+		device.SetAddress(TPiS_I2CAddress);						// default address read from EPROM
+		const uint8_t txBuffer[] = { 0x1F, 0x80 };				// command to enable read from EPROM
+		ok = device.Transfer(txBuffer, nullptr, sizeof(txBuffer), 0, TPis_I2C_timeout, false);
 	}
-	else
+
+	// Read the whole EPROM
+	uint8_t epromBuffer[32];
+	if (ok)
 	{
-		CopyBasicDetails(reply);
+		const uint8_t txBuffer[] = { 0x20 };
+		ok = device.Transfer(txBuffer, epromBuffer, sizeof(txBuffer), sizeof(epromBuffer), TPis_I2C_timeout, false);
+		const uint8_t txBuffer2[] = { 0x1F, 0x00 };				// command to disable read from EPROM
+		(void)device.Transfer(txBuffer2, nullptr, sizeof(txBuffer2), 0, TPis_I2C_timeout, true);
 	}
-	return GCodeResult::ok;
+
+	// Check the EPROM
+	uint16_t calculatedChecksum = epromBuffer[0];
+	for (size_t i = 3; i < 32; ++i)
+	{
+		calculatedChecksum += epromBuffer[i];
+	}
+	const uint16_t storedChecksum = ((uint16_t)epromBuffer[1] << 8) | (uint16_t)epromBuffer[2];
+	if (epromBuffer[0] == 3 && epromBuffer[31] == TPiS_I2CAddress && calculatedChecksum == storedChecksum)
+	{
+		// Get needed data from EPROM contents
+		TempCalibPtat25 = ((uint16_t)epromBuffer[10] << 8) | epromBuffer[11];
+		TempCalibM = ((uint16_t)epromBuffer[12] << 8) | epromBuffer[13];
+		TempCalibUo = (((uint32_t)epromBuffer[14] << 8) | epromBuffer[15]) + 32768L;
+		TempCalibUout1 = (((uint32_t)epromBuffer[16] << 8) | epromBuffer[17]) * 2;
+		TempCalibTobj1 = epromBuffer[18];
+
+		// Pre-compute some derived calibration constants
+		recipTempCalibM = 1.0/(float)TempCalibM;
+		recipCorrectedK = ((powf((float)TempCalibTobj1, 4.2) - powf(25.0 + 273.15, 4.2)) * FovAndEmissivityCorrection) / (float)(TempCalibUout1 - TempCalibUo);
+
+		ambientTemperatureDegK = objectTemperatureDegK = 0.0;
+	}
 }
 
-void TPiS_1T_1086_L5_5::Poll()
+bool TPiS_1T_1086_L5_5::TakeReading() noexcept
 {
-	//TODO
+	// Read the object and ambient temperatures from the sensor
+	const uint8_t txBuffer[] = { 0x01 };
+	uint8_t rxBuffer[4];
+	if (device.Transfer(txBuffer, rxBuffer, sizeof(txBuffer), sizeof(rxBuffer), true))
+	{
+		const uint32_t rawTpObject = ((uint32_t)rxBuffer[0] << 9) | ((uint32_t)rxBuffer[1] << 1) |((uint32_t)rxBuffer[2] >> 7);
+		const uint32_t rawTAmb = ((uint32_t)(rxBuffer[2] & 0x7F)) << 8 | (uint32_t)rxBuffer[3];
+
+		// Calculate the ambient temperature reported by the sensor in degK. See datasheet section 8.4.
+		ambientTemperatureDegK = (25.0 + 273.15) + (float)(rawTAmb - TempCalibPtat25) * recipTempCalibM;
+
+		// Calculate the object temperature. See datasheet section 8.5. We assume that LOOKUP# = 2.
+		objectTemperatureDegK = powf((rawTpObject - TempCalibUo) * recipCorrectedK + powf(ambientTemperatureDegK, 4.2), 1.0/4.2);
+		return true;
+	}
+
+	return false;
 }
 
 #endif
--- src/Hardware/Drivers/TPiS_1T_1086_L5_5.h
+++ src/Hardware/Drivers/TPiS_1T_1086_L5_5.h
@@ -12,20 +12,42 @@
 
 #if SUPPORT_TPiS_1T_1086_L5_5
 
-#include "I2CTemperatureSensor.h"
+#include <Hardware/SharedI2CClient.h>
 
-class TPiS_1T_1086_L5_5 : public I2CTemperatureSensor
+class CanMessageGenericParser;
+
+class TPiS_1T_1086_L5_5
 {
 public:
-	TPiS_1T_1086_L5_5(unsigned int sensorNum);
-	GCodeResult Configure(const CanMessageGenericParser& parser, const StringRef& reply) override;
-
-	static constexpr const char *TypeName = "tpis1t1086";
-
-	void Poll() override;
+	TPiS_1T_1086_L5_5(SharedI2CMaster& master) noexcept;
+	void Init() noexcept;
+	bool TakeReading() noexcept;
+	float GetAmbientTemperature() const noexcept { return ambientTemperatureDegK - 273.15; }
+	float GetObjectTemperature() const noexcept { return objectTemperatureDegK - 273.15; }
 
 private:
-	static SensorTypeDescriptor typeDescriptor;
+	static constexpr uint32_t TPis_I2C_timeout = 25;					// timeout in milliseconds when waiting to acquire the I2C bus
+
+	static constexpr float FovAndEmissivityCorrection = 0.45;			// Calibration factor for reduced emissivity and/or reduced FOV coverage
+
+	SharedI2CClient device;
+
+	// Calibration constants read from EPROM
+	uint16_t TempCalibPtat25;
+	uint16_t TempCalibM;
+	uint32_t TempCalibUo;
+	uint32_t TempCalibUout1;
+	uint8_t TempCalibTobj1;
+
+	// Calibration constants calculated from the above
+	float recipTempCalibM;
+	float recipCorrectedK;
+
+	// Latest readings
+	float ambientTemperatureDegK;
+	float objectTemperatureDegK;
+
+	bool initialised = false;
 };
 
 #endif
